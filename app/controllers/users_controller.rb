3fd8cfefe6b3f8ab5fcf18978d2e512e8067f871
Robert Sainton <robertsainton@outlook.com>
2023-06-14 01:52:06 -0400

    code users controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+    skip_before_action :confirm_authentication
+    
+    def show
+        if current_user
+          render json: @current_user, status: :ok 
+        else
+          render json: { error: "Not authorized" }, status: :unauthorized
+        end
+      end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: user.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def user_params
+        params.permit(:username, :age, :location, :favorite_varietal, :password, :password_confirmation)
+    end
+
 end
