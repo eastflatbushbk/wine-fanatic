63bbc79ce45e6d7275bf2e2595c34db1711ab7a2
Robert Sainton <robertsainton@outlook.com>
2023-06-14 01:46:08 -0400

    code user auth in application controler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,27 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+before_action :confirm_authentication
+
+def current_user
+  @current_user = User.find_by_id(session[:user_id])
+end
+
+private
+def confirm_authentication
+  @current_user = User.find_by_id(session[:user_id])
+  render json: { errors: ["Not Authorized"] }, status: :unauthorized unless @current_user
+end
+
+def render_not_found_response
+  render json: { errors:  ["Not found"] }, status: :not_found
+end
+
+def render_unprocessable_entity_response(exception)
+  render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+end
+
 end
